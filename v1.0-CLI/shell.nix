04cb52f042541677f63ad01796b63cdf61b61339
= <=>
2023-01-17 17:29:47 +0100

    super tp1

---
+++ v1.0-CLI/shell.nix
@@ -0,0 +1,33 @@
+{ nixpkgs ? import <nixpkgs> {}, compiler ? "default", doBenchmark ? false }:
+
+let
+
+  inherit (nixpkgs) pkgs;
+
+  f = { mkDerivation, base, hpack, lib, unordered-containers }:
+      mkDerivation {
+        pname = "voting-machine";
+        version = "0.1.0.0";
+        src = ./.;
+        isLibrary = true;
+        isExecutable = true;
+        libraryHaskellDepends = [ base ];
+        libraryToolDepends = [ hpack ];
+        executableHaskellDepends = [ base unordered-containers ];
+        testHaskellDepends = [ base ];
+        prePatch = "hpack";
+        homepage = "https://github.com/githubuser/voting-machine#readme";
+        license = lib.licenses.bsd3;
+      };
+
+  haskellPackages = if compiler == "default"
+                       then pkgs.haskellPackages
+                       else pkgs.haskell.packages.${compiler};
+
+  variant = if doBenchmark then pkgs.haskell.lib.doBenchmark else pkgs.lib.id;
+
+  drv = variant (haskellPackages.callPackage f {});
+
+in
+
+  if pkgs.lib.inNixShell then drv.env else drv
