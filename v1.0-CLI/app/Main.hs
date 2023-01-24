0a668072da9a0a976b3e084371e9da65f070619a
VM IUT <vm@iut>
2023-01-24 11:57:10 +0100

    tp

--- v1.0-CLI/app/Main.hs
+++ v1.0-CLI/app/Main.hs
@@ -1,6 +1,38 @@
-module Main (main) where
+import Data.Map (Map, empty, insert, lookup, toList)
+import Text.Printf (printf)
 
-import Lib
+type Candidat = String
+type Voter = String
+type Votes = Map Candidat Int
 
 main :: IO ()
-main = someFunc
+main = do
+  votes <- processCommand empty
+  putStrLn "Au revoir!"
+
+processCommand :: Votes -> IO Votes
+processCommand votes = do
+  putStrLn "Entrez une commande :"
+  command <- getLine
+  case words command of
+    ["voter", voter, candidat] -> do
+      case Data.Map.lookup voter votes of
+        Just _ -> putStrLn $ voter ++ " a déjà voté."
+        Nothing -> case Data.Map.lookup candidat votes of
+          Just _ -> do
+            putStrLn $ "Merci pour votre vote, " ++ voter ++ "."
+            putStrLn ( voter ++ candidat ++ show(votes))
+          Nothing -> putStrLn $ "Candidat " ++ candidat ++ " inconnu."
+      processCommand votes
+    ["voir", candidat] -> do
+      case Data.Map.lookup candidat votes of
+        Just count -> putStrLn $ candidat ++ " : " ++ show count ++ " voix."
+        Nothing -> putStrLn $ "Candidat " ++ candidat ++ " inconnu."
+      processCommand votes
+    ["voir"] -> do
+      putStrLn "Scores :"
+      mapM_ (\(candidat, count) -> putStrLn $ candidat ++ " : " ++ show count ++ " voix.") $ toList votes
+      processCommand votes
+    _ -> do
+      putStrLn "Commande inconnue."
+      processCommand votes
